55cb52a736950755a3ba57f8851e04bbabcb68a0
wangnan <wangnan1@yidian-inc.com>
2018-10-30 12:04:29 +0800

    compile

--- mvvm1/compile.js
+++ mvvm1/compile.js
@@ -7,15 +7,50 @@ function Compile(el){
   }
 }
 Compile.prototype = {
+  init: function (){
+    this.compileElement(this.$fragment);
+  },
   node2Fragment: function(node){
     let fragment = document.createDocumentFragment(), child;
     while(child = node.firstChild){
+      // 拷贝节点
       fragment.appendChild(child);
     }
     return fragment;
   },
   compileElement: function(node){
+    let _this = this,
+        childNodes = node.childNodes;
+    [].slice.call(childNodes).forEach(function(nodeItem){
+      // todo 兼容处理
+      /**
+       * innerText受到CSS样式的影响，并且不会返回隐藏元素的文本，而textContent会
+       * 改变innerText会触发重排reflow，但textContent不会
+       * textContent会获取所有元素的内容，包括<script>,<style>，然而innerText不会
+       * 
+       * innerHTML返回HTML文本，textContent性能更好，因为文本不会被解析为HTML
+       * 使用textContent可以防止XSS攻击
+       */
+      let content = nodeItem.textContent, 
+          reg = /\{\{(.*)\}\}/;
+      if(_this.isElementNode(nodeItem)){
+        // 元素节点编译
+        _this.compile(nodeItem);
+      } else if(_this.isTextElementNode(nodeItem) && reg.test(content)){
+        // 文本节点
+        _this.compileText(nodeItem, RegExp.$1);
+      }
+      // 递归子节点
+      if(nodeItem.childNodes && nodeItem.childNodes.length){
+        _this.compileElement(nodeItem);
+      }
+    });
+  },
+  compile: function(node){
     
+  },
+  compileText: function(node, content){
+
   },
   isElementNode: function(node){
     return node && node.nodeType === 1;  // 元素节点 p, div
