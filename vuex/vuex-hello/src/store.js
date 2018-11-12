fce26f48800b3a17eb48889900844b28129192b0
wangnan <wangnan1@yidian-inc.com>
2018-11-12 19:47:32 +0800

    完善

--- vuex/vuex-hello/src/store.js
+++ vuex/vuex-hello/src/store.js
@@ -5,12 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    count: 2,
+    obj: {
+      age: 16
+    }
   },
   mutations: {
-
+    add (state, n) {
+      state.count += n
+    },
+    reduce (state, n) {
+      state.count -= n
+    }
+  },
+  getters: {
+    count: state => {
+      state.count += 100
+      return state.count
+    }
   },
   actions: {
-
+    addAction ({ commit }) {
+      setTimeout(() => {
+        commit('add', 10)
+        console.log(new Date().getTime())
+      })
+      console.log('我比add提前执行', new Date().getTime())
+    },
+    reduceAction ({ commit }) {
+      commit('reduce', 10)
+    }
   }
 })
